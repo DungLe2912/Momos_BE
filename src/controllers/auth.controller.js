717e63a9edb450dcf79b1e72b4a2c566321eb833
Dung Le <qdung.le3912@gmail.com>
2024-12-18 19:24:55 +0700

    chore: update error handling middleware

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,26 +1,24 @@
 import { AuthService } from "../services/auth.service.js";
 
 export class AuthController {
-  static async login(req, res) {
+  static async login(req, res, next) {
     try {
       const data = await AuthService.login(req.body);
       return res.status(200).json(data);
     } catch (error) {
-      res.status(500).json({ message: "Login failed", error: error.message });
+      next(error);
     }
   }
 
-  static async logout(req, res) {}
+  static async logout(req, res, next) {}
 
-  static async refreshToken(req, res) {
+  static async refreshToken(req, res, next) {
     const { userId, refreshToken } = req.body;
     try {
       const data = await AuthService.refreshToken(userId, refreshToken);
       return res.status(200).json(data);
     } catch (error) {
-      res
-        .status(500)
-        .json({ message: "Get new tokens failed", error: error.message });
+      next(error);
     }
   }
 }
